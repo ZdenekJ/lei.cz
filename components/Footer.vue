853ff491357c07deaaf4c2bf14c693a73c66961d
ZdenekJ <zdenek.jasek@gmail.com>
2021-04-23 17:10:08 +0200

    Optimalizace

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,6 +2,13 @@
   <footer id="site-footer" class="text-center">
     <hr class="hr-text" />
     <span>&copy;2003 - {{ thisYear }} Lei</span>
+    <script type="text/javascript">
+             let stylesheet = document.createElement('link');
+             stylesheet.href = '//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css';
+             stylesheet.rel = 'stylesheet';
+             stylesheet.type = 'text/css';
+             document.getElementsByTagName('head')[0].appendChild(stylesheet);
+       </script>
   </footer>
 </template>
 

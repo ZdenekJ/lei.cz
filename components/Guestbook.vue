6208a08de30e1de20335fd64e7ba49f864e4246d
ZdenekJ <zdenek.jasek@gmail.com>
2021-08-29 18:01:24 +0200

    Zakladni strankovani

--- components/Guestbook.vue
+++ components/Guestbook.vue
@@ -5,24 +5,34 @@
     <div class="post" v-if="posts.length < 1">
       <h4>Jejda, zatím nikdo nic nekomentoval</h4>
     </div>
-    <div class="post" v-else v-for="post in posts" :key="post.text">
-      <h4>
-        <span v-if="post.is_author === 1" class="fa fa-user"></span
-        >{{ post.nick }} <small>- {{ dateTimeFormat(post.date) }}</small>
-      </h4>
-      <p v-html="textFormat(post.text)"></p>
+    <div v-else>
+      <Pagination
+        :active-page="activePage"
+        :pages-count="pagesCount"
+        @changePage="activePage = $event"
+      />
+      <div class="post" v-for="post in posts" :key="post.text">
+        <h4>
+          <span v-if="post.is_author === 1" class="fa fa-user"></span
+          >{{ post.nick }} <small>- {{ dateTimeFormat(post.date) }}</small>
+        </h4>
+        <p v-html="textFormat(post.text)"></p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import { dateTimeFormat } from "@/helpers/dateTimeFormater.js";
+import { Pagination } from "~/components/Pagination.vue";
 
 export default {
   name: "Guestbook",
   data() {
     return {
       posts: [],
+      activePage: 1,
+      pagesCount: 10,
     };
   },
   props: {
@@ -36,6 +46,9 @@ export default {
     },
   },
 
+  watch: {
+    activePage: "$fetch",
+  },
   async fetch() {
     const url =
       "http://" +
@@ -43,20 +56,40 @@ export default {
       ":" +
       process.env.apiPort +
       "/" +
-      this.slug;
+      this.slug +
+      "/" +
+      (this.activePage - 1);
 
     const json = await this.$axios.$get(url);
 
-    let data = this.posts;
-    data.push(...json.posts);
+    // API indexuje od 0, ale pro sanzsi praci se strankovanim je v aplikaci indexace od 1
+    this.activePage = json.page + 1;
+    this.pagesCount = json.pagesCount;
+    this.posts = [...json.posts];
   },
   methods: {
+    async getDataFromApi() {
+      const url =
+        "http://" +
+        process.env.apiHost +
+        ":" +
+        process.env.apiPort +
+        "/" +
+        this.slug +
+        "/" +
+        this.activePage;
+
+      const json = await this.$axios.$get(url);
+
+      this.activePage = json.page;
+      this.pagesCount = json.pagesCount;
+      this.posts = [...json.posts];
+    },
     dateTimeFormat(date) {
       return dateTimeFormat(date);
     },
     textFormat(str) {
       let text = str.replace(/(?:\r\n|\r|\n)/g, "<br>");
-
       return text;
     },
   },
---
+++ components/Pagination.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    Pagination
+
+    <p @click="chnagePage(activePage - 1)" v-if="activePage > 1">
+      Předchozí stránka
+    </p>
+    <p v-else>
+      Předchozí stránka
+    </p>
+    <p v-for="page in pagesCount" v-bind:key="page" @click="chnagePage(page)">
+      {{ page }}
+    </p>
+    <p @click="chnagePage(activePage + 1)" v-if="activePage < pagesCount">
+      Další stránka
+    </p>
+    <p v-else>Další stránka</p>
+    <span>{{ activePage }}</span>
+    <span>{{ pagesCount }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Pagination",
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  props: {
+    activePage: {
+      type: Number,
+      required: true,
+    },
+    pagesCount: {
+      type: Number,
+      default: true,
+    },
+  },
+
+  methods: {
+    chnagePage(page) {
+      this.$emit("changePage", page);
+    },
+  },
+};
+</script>
+
+<style lang="scss"></style>
